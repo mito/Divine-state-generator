772aefd717cbd6b373ca3871afd001041b4e2bec
mito <mito@tennismanager.sk>
2009-07-07 10:43:46 +0300

    devine init

--- src/divine-int.hh
+++
@@ -1,9 +0,0 @@
-#ifndef _DIVINE_CLUSTER_SRC_DIVINE_INT_HH
-#define _DIVINE_CLUSTER_SRC_DIVINE_INT_HH 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "divine-cluster 0.8.2"
-/* generated using gnu compiler gcc (GCC) 4.1.2 20061115 (prerelease) (Debian 4.1.1-21) */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
